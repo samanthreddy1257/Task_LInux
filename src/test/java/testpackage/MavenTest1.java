530e2910b1f4288cab949abaa043dcd37d88c217
samanthreddy1257 <92386231+samanthreddy1257@users.noreply.github.com>
2021-12-22 11:01:47 +0530

    Update MavenTest1.java

--- src/test/java/testpackage/MavenTest1.java
+++ src/test/java/testpackage/MavenTest1.java
@@ -7,7 +7,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test; 
 public class MavenTest1 {
 	public String baseUrl = "https://www.google.com/";  
-	String driverPath = "//home//ec2-user//chromedriver";  
+	String driverPath = "/usr/bin/google-chrome";  
 	public WebDriver driver ;   
 	@Test             
 	public void test() {      
